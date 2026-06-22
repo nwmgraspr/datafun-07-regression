547c15dc7cde081293150fd5cb19ec05ed0c127b
nwmgraspr <rmblamah@gmail.com>
2026-06-22 13:07:01 -0500

    Update app_penguins_case_copy.py

--- src/datafun/app_penguins_case_copy.py
+++ src/datafun/app_penguins_case_copy.py
@@ -50,7 +50,7 @@ from typing import Final  # for type hinting
 
 from datafun_toolkit.logger import get_logger, log_header
 from matplotlib.axes import Axes
-import matplotlib.pyplot as pltimport seaborn as sns
+import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 
